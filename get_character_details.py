cb01e4558d23e0f903d31a81e01e81f573d48464
Charlie Joynt <cjj1977@users.noreply.github.com>
2023-12-21 09:22:05 +0000

    download and parse JSON

--- get_character_details.py
+++ get_character_details.py
@@ -1,52 +1,86 @@
 import json
+import requests
 
-# Load the provided JSON file
-file_path = '/mnt/data/102490243.json'
+# local imports
+from dndbeyond_config import CHARACTER_PROPERTIES, DDB_BASE_URL
 
-with open(file_path, 'r') as file:
-    json_data = json.load(file)
 
-# Function to extract properties from JSON data
-def extract_properties(data, properties):
-    def get_nested_property(data, path):
+def get_character_ids(path: str = "./characters/characters.md") -> iter:
+    """
+    An iterable function that yields character IDs from the characters.md file.
+
+    We read the contents of the characters.md file and extract character IDs.
+    These will be in the form "* Character ID: <id>". We want to extract the ID
+    yield the IDs as we discover them.
+
+    :param path: The path to the characters.md file
+    :return: An iterable of character IDs
+    """
+    with open(path, "r") as file:
+        file_contents = file.read()
+
+    # Split the file contents into lines
+    lines = file_contents.split("\n")
+
+    # Extract the character IDs
+    for line in lines:
+        if line.startswith("* Character ID:"):
+            yield line.split(": ")[1]
+
+
+def _extract_properties(data: dict, properties: dict) -> dict:
+    """
+    Function to extract specific properties from JSON data.
+
+    We take a dictionary of properties and data types and extract the
+    corresponding values from the JSON data. We return a dictionary of the
+    extracted properties.
+
+    :param data: The JSON data to extract properties from
+    :param properties: A dictionary of properties and data types
+    :return: A dictionary of extracted properties
+    """
+
+    def _get_nested_property(data, path):
         try:
-            for key in path.split('/'):
-                data = data[key] if key != '*' else data[0]
+            for key in path.split("/"):
+                data = data[key] if key != "*" else data[0]
             return data
         except (KeyError, IndexError, TypeError):
             return None
 
     extracted_data = {}
     for prop, data_type in properties.items():
-        value = get_nested_property(data, prop)
-        if data_type == 'int' and value is not None:
+        value = _get_nested_property(data, prop)
+        if data_type == "int" and value is not None:
             value = int(value)
         extracted_data[prop] = value
 
     return extracted_data
 
-# list of desired properties based on the corrected paths
-desired_properties = {
-    "data/name": "str",
-    "data/gender": "str",
-    "data/faith": "str",
-    "data/age": "int",
-    "data/hair": "str",
-    "data/eyes": "str",
-    "data/skin": "str",
-    "data/height": "str",
-    "data/weight": "str",
-    "data/background/definition/shortDescription": "str",
-    "data/background/customBackground/shortDescription": "str",
-    "data/race/fullName": "str",
-    "data/race/description": "str",
-    "data/notes": "dict",
-    "data/traits": "dict",
-    "data/classes/*/definition/name": "str",
-    "data/classes/*/subclassDefinition/name": "str"
-}
 
-# Extract the data
-extracted_data = extract_properties(json_data, desired_properties)
+def get_dndbeyond_data(character_id: str) -> dict:
+    """
+    Function to get character data from D&D Beyond API. We append the character
+    ID to the base URL and make a GET request to the resulting URL. We then
+    filter the JSON response using extract_properties() and return the result.
+    """
+    url = f"{DDB_BASE_URL}{character_id}"
+    response = requests.get(url)
+    data = response.json()
+    return _extract_properties(data, CHARACTER_PROPERTIES)
 
-extracted_data
+
+def save_character(character_id, data):
+    """
+    Function to save character data to a JSON file
+    """
+    with open(f"./characters/{character_id}.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+
+if __name__ == "__main__":
+    for character_id in get_character_ids():
+        print(f"Getting character {character_id}...")
+        data = get_dndbeyond_data(character_id)
+        save_character(character_id, data)
